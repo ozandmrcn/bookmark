a6a5802ee39b648d8aa88d9417a796a2f45dd2c5
Ozan Demircan <ozandmrcn47@gmail.com>
2026-09-23 16:09:50 +0300

    feat

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,6 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
@@ -9,10 +8,13 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor(config: ConfigService) {
-    const adapter = new PrismaPg({
-      connectionString: config.get<string>('DATABASE_URL'),
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>('DATABASE_URL'),
+        },
+      },
     });
-    super({ adapter });
   }
 
   async onModuleInit() {
@@ -30,4 +32,3 @@ export class PrismaService
     ]);
   }
 }
-
